5501de5bfe5048682f46989ae92c46bc937e62b1
anDack <1160083806@qq.com>
2017-01-04 10:34:31 +0800

    adjust layout

--- app/src/main/java/com/my/andack/secretcodedome/MainActivity.java
+++ app/src/main/java/com/my/andack/secretcodedome/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
         setBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
